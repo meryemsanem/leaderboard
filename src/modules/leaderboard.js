2a9827c05db51e6571880c2eeb85d3bc3f9cd534
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-06-27 13:10:21 +0300

    Implement the basic structure of the leaderboard

---
+++ src/modules/leaderboard.js
@@ -0,0 +1,27 @@
+export const leaderboard = [
+  {
+    id: 1,
+    name: 'John',
+    score: 50,
+  },
+  {
+    id: 2,
+    name: 'Paul',
+    score: 60,
+  },
+  {
+    id: 3,
+    name: 'Michael',
+    score: 70,
+  },
+  {
+    id: 4,
+    name: 'Laura',
+    score: 80,
+  },
+  {
+    id: 5,
+    name: 'James',
+    score: 90,
+  },
+];
